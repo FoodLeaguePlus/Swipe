e7817715a1d6217132349fa219972d6e82306a8d
charlenenicer <charlenenicer@gmail.com>
2019-02-02 10:13:30 -0800

    added new frame

---
+++ testingTinderSwipe/directMessage.swift
@@ -0,0 +1,33 @@
+//
+//  directMessage.swift
+//  testingTinderSwipe
+//
+//  Created by Charlene Nicer on 2019-02-02.
+//  Copyright © 2019 Nicky. All rights reserved.
+//
+
+import UIKit
+
+class directMessage: UIViewController {
+
+    @IBAction func onClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
